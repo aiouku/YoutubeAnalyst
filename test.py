5d28f7fd4f0109f0b382bc776796bb01d82da29f
aiouku <keigames2005@gmail.com>
2025-07-23 19:10:52 +0900

    Fix test.py

--- test.py
+++ test.py
@@ -4,8 +4,7 @@ from datetime import datetime, timedelta
 n_input = input("読み取る動画数は？（空で全件）: ")
 N = int(n_input) if n_input.strip() else None 
 D = 0.7 #視聴時間
-path = 'watch-history-short.html'
-#path = 'watch-history.html'
+path = 'watch-history.html'
 
 with open(path, encoding='utf-8') as f:
     soup = BeautifulSoup(f, 'lxml')
@@ -57,13 +56,6 @@ else:
     print("視聴日時が十分にありません。")
 
 print(f"総視聴時間 (動画数:{N} ): {total_duration}")
-'''
-last = int(input("視聴履歴を表示する？(0: No , 1: Yes)"))
-if last == 1:
-    print("\n--- 視聴履歴一覧 ---")
-    for title, dt in watch_data:
-        print(f"{dt} - {title}")
-'''
 show_links = int(input("動画リンクを表示しますか？(0: No , 1: Yes): "))
 if show_links == 1:
     print("\n--- YouTubeリンク一覧 ---")
